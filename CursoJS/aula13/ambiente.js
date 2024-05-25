6017adbbe4e390fa5db30e41865945241ee058af
MizaelSiebenSilvano <96353670+mizaelsiebensilvano@users.noreply.github.com>
2024-05-25 12:15:20 -0300

    exercicio JS
    exercicio de contagem

--- CursoJS/aula13/ambiente.js
+++ CursoJS/aula13/ambiente.js
@@ -1,24 +1,16 @@
-/* var c = 1
+var c = 1
 do {
     console.log(`Passo ${c}`)
     c++
-} while (c <= 6)
- */   
+} while (c <= 600)
 
-
+/*
 var c = 1
 while (c <= 6) {
     console.log(`Passo ${c}`)
     c++
-} 
-
-
-
-
-
-
-
-
+}
+*/
 
 /*
 console.log('Tudo bem?')
---
+++ CursoJS/aula14/ambiente.js
@@ -0,0 +1,20 @@
+console.log('Vai começar...')
+for (var c = 1; c <= 5 ; c++){
+    console.log(c)
+}
+console.log('FIM')
+
+/*
+var c = 1
+while (c <= 5) {
+    console.log(c)
+    c++
+}
+*/
+
+/* 
+var c = 1
+for (var c = 1; c <= 10; c++) {
+    console.log(c)
+}
+*/
