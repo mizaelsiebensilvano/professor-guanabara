92a56a75da268cf42c77db6deb768016716d5d7e
MizaelSiebenSilvano <96353670+mizaelsiebensilvano@users.noreply.github.com>
2024-05-25 10:05:00 -0300

    exercicio JS

--- CursoJS/aula13/ambiente.js
+++ CursoJS/aula13/ambiente.js
@@ -1,16 +1,16 @@
-var c = 1
+/* var c = 1
 do {
     console.log(`Passo ${c}`)
     c++
 } while (c <= 6)
-    
+ */   
 
 
-/* var c = 1
+var c = 1
 while (c <= 6) {
     console.log(`Passo ${c}`)
     c++
-} */
+} 
 
 
 
@@ -20,9 +20,8 @@ while (c <= 6) {
 
 
 
-
-
-/* console.log('Tudo bem?')
+/*
+console.log('Tudo bem?')
 console.log('Tudo bem?')
 console.log('Tudo bem?')
 console.log('Tudo bem?')
